c40bb44c31295cdef6cc1f47e482bebae8bcf786
fenyiwudian <674702221@qq.com>
2020-11-11 17:39:39 +0800

    produce sort list number render bug

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -6,11 +6,12 @@ import { Core, Inner, Decline, Hub } from '../../../lib/demo'
 import { a } from '../../../lib/a';
 
 export default () => {
-  
+
   console.log(Core, Inner, Decline, Hub, a);
   console.log(process.env);
   return (
     <View className={styles.app}>
+      <SortList></SortList>
       <View className={styles.header}>
         <Image
           src="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ"
@@ -25,3 +26,56 @@ export default () => {
     </View>
   );
 };
+
+const initialList = [
+  {
+    text: '第一项',
+  },
+  {
+    text: '第二项',
+  },
+  {
+    text: '第三项',
+  },
+  {
+    text: '第四项',
+  },
+  {
+    text: '第五项',
+  }
+];
+class SortList extends React.Component {
+  state = {
+    list: initialList,
+  }
+  resort(index, step) {
+    const { list } = this.state;
+    const targetIndex = index + step;
+    if (targetIndex < 0 || targetIndex > list.length - 1) {
+      return;
+    }
+    const newList = [...list];
+    const swap = newList[targetIndex];
+    newList[targetIndex] = newList[index];
+    newList[index] = swap;
+    this.setState({ list: newList });
+  }
+  render() {
+    const { list } = this.state;
+    return <View>
+      <View>Remax Sort List:</View>
+      {list.map((item, index) => {
+        return <View key={item.text} style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <View>{index}</View>
+          <View>{index + item.text}</View>
+          {index > 0
+            ? <View onTap={() => this.resort(index, -1)}>上移</View>
+            : <View>N/A</View>}
+          {index < list.length - 1
+            ? <View onTap={() => this.resort(index, 1)}>下移</View>
+            : <View>N/A</View>}
+        </View>
+      })}
+    </View>
+  }
+}
